21b8257bd28d53020c2a2110cc28fb44cb251ca7
Salmankhancodes <117salmankhan@gmail.com>
2021-08-27 01:38:18 +0530

    Initialized React app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Switch>
+          <Route path='/' component={Home} exact></Route>
+          <Route path='/about' component={About}></Route>
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default App
