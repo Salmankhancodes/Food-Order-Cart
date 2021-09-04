511567a2082339819e6fe1153ec21c4312b5866e
Salmankhancodes <117salmankhan@gmail.com>
2021-09-04 17:14:02 +0530

    delete product from cart and grand total update

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { CartContext } from '../CartContext'
 import { useContext } from 'react'
+
 const Cart = () => {
+  let total = 0
   const [products, setProducts] = useState([])
+  const [priceFetched, togglePriceFetched] = useState(false)
   const { cart, setCart } = useContext(CartContext)
 
   useEffect(() => {
     if (!cart.items) {
       return
     }
-    fetch('/api/products/cart-items', {
+    if (priceFetched) {
+      return
+    }
+    fetch('api/products/cart-items', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -19,6 +25,7 @@ const Cart = () => {
       .then((response) => response.json())
       .then((products) => {
         setProducts(products)
+        togglePriceFetched(true)
       })
   }, [cart])
 
@@ -44,13 +51,30 @@ const Cart = () => {
     setCart(_cart)
   }
 
+  const getSum = (productID, price) => {
+    const sum = price * getSum(productID)
+    total += sum
+    return sum
+  }
+  const handleDelete = (productID) => {
+    const _cart = { ...cart }
+    const qty = _cart.items[productID]
+    delete _cart.items[productID]
+    _cart.total -= qty
+    setCart(_cart)
+    const updatedProductList = products.filter(
+      (product) => product._id !== productID
+    )
+    setProducts(updatedProductList)
+  }
+
   return (
     <div className='container mx-auto lg:w-1/2 w-full pb-24'>
       <h1 className='my-12 font-bold'> Cart Items</h1>
       <ul>
         {products.map((product) => {
           return (
-            <li className='mb-12'>
+            <li className='mb-12' key={product._id}>
               <div className='flex item-center justify-between'>
                 <div className='flex item-center'>
                   <img src={product.image} alt='cart_item' className='h-16' />
@@ -75,8 +99,13 @@ const Cart = () => {
                     +
                   </button>
                 </div>
-                <span>Rs {product.price}</span>
-                <button className='bg-red-500 px-4 py-2 rounded-full leading-none text-white'>
+                <span>Rs {getSum(product._id, product.price)}</span>
+                <button
+                  onClick={() => {
+                    handleDelete(product._id)
+                  }}
+                  className='bg-red-500 px-4 py-2 rounded-full leading-none text-white'
+                >
                   Delete
                 </button>
               </div>
@@ -86,7 +115,7 @@ const Cart = () => {
       </ul>
       <hr className='my-6' />
       <div className='text-right'>
-        <b>Grand Total:</b> Rs 1200
+        <b>Grand Total:</b> Rs {total}
       </div>
       <div className='text-right mt-6'>
         <button className='bg-yellow-500 px-4 py-2 rounded-full leading-none'>
